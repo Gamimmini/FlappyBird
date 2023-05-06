e2b41349883275345dc508d52ce52f8f3026499a
Gamimmini <hnthanh922@gmail.com>
2023-05-07 02:10:51 +0700

    first commit

---
+++ Assets/FlappyBird.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FlappyBird : MonoBehaviour
+{
+ Rigidbody2D rg;
+
+ public GameObject gameOverObj;
+
+ public float speed;
+
+ private void Start() {
+   Time.timeScale = 1;
+    rg = GetComponent<Rigidbody2D>();
+
+ }
+ private void Update() {
+    if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+    {
+        rg.AddForce(Vector2.up * speed);
+    } 
+ }
+
+   public void resetGame()
+   {
+      SceneManager.LoadScene(0);
+   }
+
+   void GameOver()
+   {
+      gameOverObj.SetActive(true);
+      Time.timeScale = 0;
+   }
+
+
+   private void OnCollisionEnter2D(Collision2D collision)
+   {
+      GameOver();
+   }
+}
+ 
+
+
+
